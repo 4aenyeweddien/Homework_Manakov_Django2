277e80ca0f40f2883e2dc10ee52e2e1c3cd7a69d
4aenyeweddien <manackov.vitek@gmail.com>
2025-09-29 23:25:26 +0300

    редактирование README

--- catalog/management/commands/add_product.py
+++ catalog/management/commands/add_product.py
@@ -1,5 +1,5 @@
+from django.core.management import call_command
 from django.core.management.base import BaseCommand
-
 from catalog.models import Product, Category
 
 
@@ -9,30 +9,38 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         Product.objects.all().delete()
-        self.stdout.write(self.style.SUCCESS('Все существующие продукты удалены'))
+        Category.objects.all().delete()
+        self.stdout.write(self.style.SUCCESS('Все существующие продукты и категории удалены'))
 
-        category, _ = Category.objects.get_or_create(
-            name="Пылесос", description="для уборки дома"
-        )
+        call_command('loaddata', 'category_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Фикстура категорий загружена'))
 
-        products = [
-            {
-                "name": "Philips",
-                "description": "беспроводной с подсветкой и функцией мойки пола",
-                "category": category,
-                "price": 70000,
-            },
-            {
-                "name": "Bosh",
-                "description": "длинна провода 10 метров, сьемные насадки",
-                "category": category,
-                "price": 40000,
-            }
-        ]
+        call_command('loaddata', 'product_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Фикстуры продуктов загружены'))
 
-        for product_data in products:
-            product, created = Product.objects.get_or_create(**product_data)
-            if created:
-                self.stdout.write(self.style.SUCCESS(f'Добавление продукта успешно:{product.name}'))
-            else:
-                self.stdout.write(self.style.WARNING(f'Продукт уже добавлен:{product.name}'))
+
+        # category, _ = Category.objects.get_or_create(
+        #     name="Пылесос", description="для уборки дома"
+        # )
+        #
+        # products = [
+        #     {
+        #         "name": "Philips",
+        #         "description": "беспроводной с подсветкой и функцией мойки пола",
+        #         "category": category,
+        #         "price": 70000,
+        #     },
+        #     {
+        #         "name": "Bosh",
+        #         "description": "длинна провода 10 метров, сьемные насадки",
+        #         "category": category,
+        #         "price": 40000,
+        #     }
+        # ]
+        #
+        # for product_data in products:
+        #     product, created = Product.objects.get_or_create(**product_data)
+        #     if created:
+        #         self.stdout.write(self.style.SUCCESS(f'Добавление продукта успешно:{product.name}'))
+        #     else:
+        #         self.stdout.write(self.style.WARNING(f'Продукт уже добавлен:{product.name}'))
