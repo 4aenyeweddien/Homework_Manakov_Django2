f103dfdc3bd8fccf98744a3849d3c4769c0fc8ea
4aenyeweddien <manackov.vitek@gmail.com>
2025-11-09 23:08:47 +0300

    создано новое приложение Blog

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('blogs/', include('blog.urls')),
     path("", include("catalog.urls", namespace="catalog")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
